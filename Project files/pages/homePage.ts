ee3050e3d6ffeed0681b7ad96396817a19479468
simtenefeoglu <simten.efeoglu@testifi.io>
2025-07-22 12:40:24 +0300

    first commit

---
+++ Project files/pages/homePage.ts
@@ -0,0 +1,45 @@
+import { Page,Locator } from "@playwright/test";
+import { assert } from "console";
+
+
+export class HomePage {
+
+  private readonly page: Page;
+  private readonly loginSubmitButton: Locator;
+  private readonly cardTitles: Locator;
+
+
+
+  constructor(page: Page) {
+    this.page = page;
+    this.loginSubmitButton = page.getByTestId('login__submit');
+    // Locator can handle multiple elements and provides methods like .first(), .count(), .nth(), etc.
+    this.cardTitles = page.locator('mat-card-title');
+    
+  }
+
+async getCardTitleTexts(): Promise<string[]> {
+      
+        
+        await this.cardTitles.first().waitFor({ state: 'visible' });
+        
+        try {
+            const count = await this.cardTitles.count();
+            console.log('Number of elements found inside the method:', count);
+
+            const titles: string[] = [];
+            for (let i = 0; i < count; i++) {
+                const text = await this.cardTitles.nth(i).textContent();
+                if (text) titles.push(text.trim());
+                console.log(`Element ${i + 1} text:`, text);
+            }
+            
+            console.log('Final titles array:', titles);
+            return titles;
+            
+        } catch (error) {
+            console.error('Error getting card titles:', error);
+            return [];
+        }
+    }
+}
---
+++ Project files/pages/loginPage.ts
@@ -0,0 +1,32 @@
+import { Page,Locator } from "@playwright/test";
+
+export class LoginPage {
+    // Define locators for the login page elements
+    // should stay in class scope to be reusable in other tests but must be readonly
+    // This allows us to use these locators in methods without needing to redefine them each time
+  private readonly page: Page;
+  private readonly usernameInput: Locator;
+  private readonly passwordInput: Locator;
+  private readonly loginSubmitButton: Locator;
+private readonly errorMassage: Locator;
+
+  // Constructor to initialize the locators
+  constructor(page: Page) {
+    this.page = page;
+    this.usernameInput = page.getByRole("textbox", { name: "Username" });
+    this.passwordInput = page.getByRole("textbox", { name: "Password" });
+    this.loginSubmitButton = page.getByTestId("login__submit");
+    this.errorMassage = page.getByTestId('cdk-overlay-1').getByText('Username or password are wrong');
+
+  }
+get errorMessage(): Locator {
+    return this.page.locator('simple-snack-bar')
+  }
+  // Method to perform page actions
+  //we can specify promise return type to make it clear that this method is asynchronous
+  async loginToThePetstore(username: string, password: string): Promise<void> {
+    await this.usernameInput.fill(username);
+    await this.passwordInput.fill(password);
+    await this.loginSubmitButton.click();
+  }
+}
